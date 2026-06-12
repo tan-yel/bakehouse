9e2422093b266d89bdc3802fdceed14f6c91672a
tan-yel <tanyelkemal@gmail.com>
2026-06-12 11:29:20 +0100

    pw testing

--- bakehouse-app/test/playwright/basic-example.spec.cjs
+++ bakehouse-app/test/playwright/basic-example.spec.cjs
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
 test('homepage loads', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('https://tanyel-kemal-bakehouse.cta-training.academy');
   await expect(page.locator('body')).toContainText('Bakehouse');
 });
